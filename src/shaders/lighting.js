b2e61056e2d8aa39947f27d1cc3e89fb44aaff45
Eva Schiller <eva_schiller@brown.edu>
2022-12-06 17:07:01 -0500

    add cube

--- src/shaders/lighting.js
+++ src/shaders/lighting.js
@@ -1,6 +1,6 @@
 const VS_LIGHTING = `#version 300 es
     layout(location=0) in vec3 vertexPosition;
-    // layout(location=1) in vec3 vertexNormal;
+    layout(location=1) in vec3 vertexNormal;
 
     out vec4 worldPosition;
     // out vec3 worldNormal;
